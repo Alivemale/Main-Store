3a9334d7248afd4b334a6c2136a1d322da76f890
OMA <seanishaku@live.com>
2022-10-06 06:05:39 +0100

    added-list-srcdata

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import mainLogo from "../img/logo1.jpg"
+import data from "./data";
+
 
 function App() {
   return (
@@ -13,6 +15,29 @@ function App() {
             </a>
           </div>
         </nav>
+
+        <main>
+
+        {data.products.map(product => (
+            <div className="pro">
+                    <img src={product.image.img} alt={product.name} />
+                    <div class="des">
+                        <span>{product.brand}</span>
+                        <h5>{product.name}</h5>
+                        <div class="star">
+                          {/* {Rating.render({
+                            value: product.stars,
+                            text: `${product.numReviews  } Reviews`,
+                          })} */}
+                        </div>
+                        <h4>${product.price}</h4>
+                    </div>
+                    <a href="#"><i class="fal fa-shopping-cart cart"></i></a>
+            </div>
+          ))}
+        
+
+        </main>
 
     </div>
   );
